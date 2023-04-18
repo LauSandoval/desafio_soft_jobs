ea7454d3f0ca7103352e23caef0d6d67462b0d5f
Laura Sandoval <lausandoval1409@gmail.com>
2023-04-17 23:00:34 -0300

    second commit

--- backend/consultas.js
+++ backend/consultas.js
@@ -1,5 +1,5 @@
 const {Pool} = require('pg')
-
+const bcrypt = require('bcryptjs');
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
@@ -11,10 +11,37 @@ const pool = new Pool({
 
 const registrarusuarios = async (email, password, rol, lenguaje) => {
     const consulta = "INSERT INTO usuarios VALUES (DEFAULT, $1, $2, $3, $4)"
-    const values = [email, password,rol, lenguaje]
+    const passwordEncrypted = bcrypt.hashSync(password)
+    const values = [email, passwordEncrypted,rol, lenguaje]
     await pool.query(consulta, values)
 
 }
 
-module.exports = {registrarusuarios}
+const loginusuario = async (email, password)=>{
+    const consulta = "SELECT * FROM usuarios values WHERE email = $1"
+    const values = [email]
+    const {rows:[usuario], rowsCount}= await pool.query(consulta,values)
+    const {password:passwordEncrypted} = usuario
+    const passwordCorrecta = bcrypt.compareSync(password,passwordEncrypted)
+    
+    if (!passwordCorrecta) {
+        throw {code:401,message:"email is not a valid"}
+    }
+}
+
+
+const obtenerData = async(email)=>{
+
+    const consulta = "SELECT * FROM usuarios WHERE email = $1"
+    const values = [email]
+    const {rows:[usuario],rowsCount} = await pool.query(consulta,values)
+    // if (!rowsCount) {
+    //     throw {code:401,message:"email is not a valid"}
+    // }
+    delete usuario.password
+    return usuario
+
+}
+
+module.exports = {registrarusuarios, loginusuario, obtenerData}
 
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
-const {registrarusuarios} = require('./consultas')
+const {registrarusuarios, loginusuario, obtenerData} = require('./consultas')
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +11,24 @@ app.post("/usuarios",async (req, res) => {
     const {email, password, rol, lenguaje} = req.body
     await registrarusuarios(email, password, rol, lenguaje)
     res.send('Se creó usuario')
-}); 
+});
+
+app.post("/login",async(req, res)=>{
+    const {email,password} = req.body
+    await loginusuario(email,password)
+    const token = jwt.sign({email},"az_AZ")
+    res.send(token)
+})
+
+app.get("/usuarios", async (req, res)=>{
+    const authorization = req.header("Authorization");
+    const token = authorization.split("Bearer ")[1];
+    console.log(token)
+    jwt.verify(token,"az_AZ")
+    const {email} = jwt.decode(token)
+    const datos = await obtenerData(email)
+    res.json(datos)
+})
 
 
 app.listen(3000, () => {
